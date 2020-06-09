ad875c517daaae2072a5f1d409400b3ef0679531
nelsjako <nelsjako@oregonstate.edu>
2020-06-09 13:58:51 -0700

    added endpoints for assignments, missing auth checks

---
+++ api/assign.js
@@ -0,0 +1,135 @@
+const router = require('express').Router();
+
+const { validateAgainstSchema } = require('../lib/validation');
+const { requireAuthentication, authRole, generateAuthToken } = require('../lib/auth');
+const { AssignmentSchema, SubmissionSchema, insertNewAssign, getAssignById, updateAssignById, removeAssignById, insertNewSubmissionById } = require('../models/assign');
+
+router.post('/', requireAuthentication, async (req, res) => {
+    if(validateAgainstSchema(req.body, AssignmentSchema)) {
+        //TODO: Validate the user is authorized
+        try {
+            const id = await insertNewAssign(req.body);
+            res.status(201).send({
+                id: id
+            });
+        } catch (err) {
+            res.status(500).send({
+                message: "Error inserting new assignment. Try again later.",
+                error: err
+            });
+        }
+    } else {
+        res.status(403).send({
+            error: "The request body was either not present or did not contain a valid Assignment object."
+        });
+    }
+});
+
+router.get('/:id', requireAuthentication, async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const assign = await getAssignById(id);
+        if (course) {
+            res.status(200).send({
+                assign: assign
+            });
+        } else {
+            res.status(404).send("Specified Assignment `id` not found.")
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({
+            message: "An internal server error occurred.",
+            error: err
+        });
+    }
+})
+
+router.patch('/:id', async (req, res, next) => {
+    if (validateAgainstSchema(req.body, AssignmentSchema)){
+        try {
+            const id = parseInt(req.params.id);
+            const existingAssign = await getAssignById(id);
+            if(existingAssign) {
+                const updateSuccessful = await updateAssignById(id, req.body);
+                if (updateSuccessful) {
+                    res.status(200).send("Success");
+                } else {
+                    res.status(400).send({
+                        error: "The request body was either not present or did not contain any fields related to Assignment objects."
+                    });
+                }
+            } else {
+                res.status(404).send({
+                    error: "Specified Assignment `id` not found."
+                });
+            }
+        } catch (err) {
+            res.status(500).send({
+                error: "An internal server error occurred.",
+                error: err
+            });
+        }
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deleteSuccessful = await removeAssignById(parseInt(req.params.id));
+        if (deleteSuccessful) {
+            res.status(204).send("Success");
+        } else {
+            res.status(404).send({
+                error: "Specified Assignment `id` not found."
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            error: "An internal server error occurred.",
+            error: err
+        })
+    }
+});
+
+router.get('/:id/submissions', async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const assign = await getAssignById(id);
+        if (assign) {
+            res.status(200).send({
+                submissions: assign.submissions
+            });
+        } else {
+            res.status(404).send({
+                error: "Specified Assignment `id` not found."
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            error: "An internal server error occurred.",
+            error: err
+        })
+    }
+})
+
+router.post('/:id/submissions', async (req, res, next) => {
+    if (validateAgainstSchema(req.body, SubmissionSchema)) {
+        try {
+            const id = await insertNewSubmissionById(req.body);
+            res.status(201).sned({
+                id: id
+            });
+        } catch (err) {
+            res.status(500).send({
+                message: "Error inserting new submission. Try again later.",
+                error: err
+              });
+        }
+    } else {
+        res.status(400).send({
+            error: "The request body was either not present or did not contain a valid Submission object."
+        })
+    }
+})
+
+module.exports = router;
---
+++ models/assign.js
@@ -0,0 +1,84 @@
+const { ObjectId } = require('mongodb')
+const { extractValidFields } = require('../lib/validation');
+
+const AssignmentSchema = {
+    courseId: { required: true },
+    title: { required: true },
+    points: { required: true },
+    due: { required: true }
+};
+exports.AssignmentSchema = this.AssignmentSchema;
+
+const SubmissionSchema = {
+    assignmentId: { required: true },
+    studentId: { required: true },
+    timestamp: { required: true },
+    file: { required: true }
+}
+exports.SubmissionSchema = this.SubmissionSchema
+
+exports.insertNewAssign = async function (assign) {
+    const assignToInsert = extractValidFields(assign, AssignmentSchema);
+
+    const db = getDBReference();
+    const cellection = db.collection('assignments');
+    const result = await collection.insertOne(assignToInsert);
+    return result.insertedId
+}
+
+exports.getAssignById = async (id) => {
+    const db = getDBReference();
+    const collection = db.collection('assignments');
+    if (!ObjectId.isValid(id)) {
+        return null;
+    } else {
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .project({ sumbissions: 0 })
+            .toArray();
+
+        return results[0];
+    }
+}
+
+exports.updateAssignById = async function (id, course) {
+    const db = getDBReference();
+    const collection = db.collection('assignments');
+
+    if (!ObjectId.isValid(id)) {
+        return null;
+    } else {
+        assign = extractValidFields(assign, AssignmentSchema);
+        const results = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: assign }
+        );
+        return results.matchedCount > 0;
+    }
+}
+
+exports.removeAssignById = async function (id) {
+    const db = getDBReference();
+    const collection = db.collection('assignments');
+
+    if(!ObjectId.isValid(id)) {
+        return null;
+    } else {
+        const results = await collection.deleteOne(
+            { _id: new ObjectId(id) }
+        );
+        return results.deletedCount > 0;
+    }
+}
+
+exports.insertNewSubmissionById = async function (id, sumbission) {
+    const subToInsert = extractValidFields(sumbission, SubmissionSchema);
+    const db = getDBReference();
+    const collection = db.collection('submissions');
+    
+    subToInsert.assignmentId = id;
+    const result = await collection.insertOne(subToInsert);
+    return 
+
+    }
+}
